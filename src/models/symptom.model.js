3233afb9f97d088ad8332cf89e26875e7d35283c
Tamaghna Choudhuri <tamaghnachoudhuri@Tamaghnas-Laptop.local>
2024-03-11 02:53:29 +0530

    Model Changes 11 march 2:53am

--- src/models/symptom.model.js
+++ src/models/symptom.model.js
@@ -1,2 +1,29 @@
-const mongoose =require(mongoose);
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+
+const symptomSchema = new mongoose.Schema({
+  //to ask about the entry into the database if there is a 
+    name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  severity: {
+    type: Number,
+    required: true
+  },
+  duration: {
+    type: String,
+    required: true
+  },
+  // You can add more fields as needed
+});
+
+// Create a Mongoose model based on the schema
+const Symptom = mongoose.model('Symptom', symptomSchema);
+
+module.exports = Symptom;
