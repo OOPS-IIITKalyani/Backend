ef79f08fe51ee7e0e24cccd964fb5ee8f416f1df
Samyxandz <tamaghna.official@gmail.com>
2024-03-11 03:00:38 +0530

    Models Update 2:58am

--- src/models/symptom.model.js
+++ src/models/symptom.model.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 
 const symptomSchema = new mongoose.Schema({
-  //to ask about the entry into the database if there is a 
+  //to ask about the entry into the database if there is a specific format
     name: {
     type: String,
     required: true,
@@ -20,10 +20,13 @@ const symptomSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  createdAt: {
+    type: Date,
+    default: Date.now // Automatically set the current timestamp when a document is created
+  }
   // You can add more fields as needed
 });
 
 // Create a Mongoose model based on the schema
 const Symptom = mongoose.model('Symptom', symptomSchema);
-
 module.exports = Symptom;
--- src/models/symptoms.model.js
+++ src/models/symptoms.model.js
@@ -1,3 +1,16 @@
-const mongoose =require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
+
+
+const symptomsModelSchema = new mongoose.Schema({
+    symptoms: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Symptom' // Reference to the Symptom model
+      }],
+  // Add other fields related to the symptoms model
+});
+
+// Create a Mongoose model based on the schema
+const SymptomsModel = mongoose.model('SymptomsModel', symptomsModelSchema);
+
+module.exports = SymptomsModel;
 
