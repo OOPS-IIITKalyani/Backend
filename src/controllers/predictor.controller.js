9723f4671c09d12ddd954af00f561dbd4ea96079
Samyxandz <tamaghna.official@gmail.com>
2024-03-27 02:24:36 +0530

    Revert "local git problems"
    This reverts commit aac6aab58ee39ca31d64c38a0afdb86069c5c7d1.

--- src/controllers/predictor.controller.js
+++ src/controllers/predictor.controller.js
@@ -6,13 +6,13 @@ const ApiResponse = require("../utils/ApiResponse")
 
 const Predictor = asyncHandler(async (req, res,) => {
     try {
-        const { userSymptoms, age ,patientData} = req.body;
-        console.log(patientData);
+        const { userSymptoms, age } = req.body;
+
         if (!userSymptoms || !age) {
             throw new ApiError(400,'User symptoms and age are required');
         }
         const result = analyzeUserSymptoms(userSymptoms, age);
-
+        
         res.status(200).json(result);
     } catch (error) {
         throw new ApiError(500, "Something went wrong while analyzing user symptoms")
